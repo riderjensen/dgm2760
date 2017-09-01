ed4aa116601880a7a1e84f251d78efdbb48078a4
Unknown <riderjensen1@gmail.com>
2017-08-31 20:56:21 -0600

    Updated CSS

--- startJS/js/main.js
+++ startJS/js/main.js
@@ -2,13 +2,14 @@
 /*jslint browser:true */
 "use strict()";
 var userName = prompt("What is your name?");
+var neededDate = new Date();
+var neededMonth = new Date();
+var neededYear = new Date();
 
 document.getElementById("company").innerHTML = "Joe's Bed and Breakfast";
 document.getElementById("slogan").innerHTML = "For the comfort and rest you deserve";
-var neededDate = new Date();
-document.getElementById("date").innerHTML = neededDate.getDate();
-var neededMonth = new Date();
-document.getElementById("month").innerHTML = neededMonth.getMonth();
-var neededYear = new Date();
-document.getElementById("year").innerHTML = neededYear.getFullYear();
-document.getElementById("userName").innerHTML = userName;
+
+
+document.getElementById("userName").innerHTML = "Hello, " + userName + "! Welcome to the best bed and breakfast you will ever have.";
+
+document.getElementById("Money").innerHTML = "The date is " + neededMonth.getMonth() + "/" + neededDate.getDate() + "/" + neededYear.getFullYear();
