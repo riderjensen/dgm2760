7d695e7512387ad3806947051cfbdc2c71ba559e
Unknown <riderjensen1@gmail.com>
2017-10-02 19:15:26 -0600

    Attempt at JavaScript Module 9, Bluehost doesnt update fast enough

--- Module 9 - Menu Activation/ActivateMenuStart/js/main.js
+++ Module 9 - Menu Activation/ActivateMenuStart/js/main.js
@@ -1,1 +1,30 @@
 // JavaScript Document
+"use strict()";
+
+
+var hrefLocation = location.href;
+var pageLocation = hrefLocation.split('#', '?');
+var pageName = pageLocation[pageLocation.length-1];
+console.log(pageLocation);
+console.log(pageName);
+
+
+var menuHrefLocation = document.querySelectorAll("ul#mainmenu li");
+var menuLocationSplit = menuHrefLocation.split('#', '?');
+var menuName = menuLocationSplit[menuLocationSplit.length-1];
+console.log(menuLocationSplit);
+console.log(menuName);
+
+
+for(var i = 0; i < menuLocationSplit.length; i++){
+
+	if(pageName == menuLocationSplit[i]){
+		menuLocationSplit[i].parentNode.className = "active";
+		if (menuLocationSplit[i].parentNode.parentNode.parentNode.className == "") {}
+			menuLocationSplit[i].parentNode.parentNode.parentNode.className = "parent";
+	}
+	else{
+		menuLocationSplit[i].parentNode.className="";
+	}
+
+}
