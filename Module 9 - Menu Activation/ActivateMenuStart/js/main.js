c7029ec79b6716f2d877de689f441dfefe660dfe
Unknown <riderjensen1@gmail.com>
2017-10-03 17:06:44 -0600

    Both arrays and loop working, need to fix colors and classes activating

--- Module 9 - Menu Activation/ActivateMenuStart/js/main.js
+++ Module 9 - Menu Activation/ActivateMenuStart/js/main.js
@@ -3,28 +3,26 @@
 
 
 var hrefLocation = location.href;
-var pageLocation = hrefLocation.split('#', '?');
+var pageLocation = hrefLocation.split('/');
 var pageName = pageLocation[pageLocation.length-1];
 console.log(pageLocation);
 console.log(pageName);
 
 
 var menuHrefLocation = document.querySelectorAll("ul#mainmenu li");
-var menuLocationSplit = menuHrefLocation.split('#', '?');
-var menuName = menuLocationSplit[menuLocationSplit.length-1];
-console.log(menuLocationSplit);
-console.log(menuName);
+console.log(menuHrefLocation);
 
 
-for(var i = 0; i < menuLocationSplit.length; i++){
-
-	if(pageName == menuLocationSplit[i]){
-		menuLocationSplit[i].parentNode.className = "active";
-		if (menuLocationSplit[i].parentNode.parentNode.parentNode.className == "") {}
-			menuLocationSplit[i].parentNode.parentNode.parentNode.className = "parent";
+for(var i = 0; i < menuHrefLocation.length; i++){
+	var myMenuMatch = menuHrefLocation[i].firstChild.getAttribute('href');
+	console.log(myMenuMatch);
+	if(pageName == myMenuMatch){
+		menuHrefLocation[i].parentNode.className = "active";
+		if (menuHrefLocation[i].parentNode.parentNode.parentNode.className == "") {}
+			menuHrefLocation[i].parentNode.parentNode.parentNode.className = "parent";
 	}
 	else{
-		menuLocationSplit[i].parentNode.className="";
+		menuHrefLocation[i].parentNode.className="";
 	}
 
 }
