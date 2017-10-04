df4cc367220e30a85ac3ff29e61b60ee378e7801
Unknown <riderjensen1@gmail.com>
2017-10-03 22:31:22 -0600

    Module 9 Complete

--- Module 9 - Menu Activation/ActivateMenuStart/js/main.js
+++ Module 9 - Menu Activation/ActivateMenuStart/js/main.js
@@ -5,24 +5,21 @@
 var hrefLocation = location.href;
 var pageLocation = hrefLocation.split('/');
 var pageName = pageLocation[pageLocation.length-1];
-console.log(pageLocation);
-console.log(pageName);
 
 
 var menuHrefLocation = document.querySelectorAll("ul#mainmenu li");
-console.log(menuHrefLocation);
 
 
 for(var i = 0; i < menuHrefLocation.length; i++){
 	var myMenuMatch = menuHrefLocation[i].firstChild.getAttribute('href');
-	console.log(myMenuMatch);
 	if(pageName == myMenuMatch){
 		menuHrefLocation[i].className = "active";
-		if (menuHrefLocation[i].parentNode.parentNode.className == "") {}
-			menuHrefLocation[i].parentNode.parentNode.className = "parent";
+		if (menuHrefLocation[i].parentNode.parentNode.className == "" && (pageName == "hammer.php" || pageName == "drill.php" )){ 
+		menuHrefLocation[i].parentNode.parentNode.className = "parent";
+			}
 	}
 	else{
-		menuHrefLocation[i].parentNode.className="";
+		menuHrefLocation[i].className="";
 	}
 
 }
