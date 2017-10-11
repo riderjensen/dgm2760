1fcacc9d9745159fc1772345107f1fe93155b7f5
Unknown <riderjensen1@gmail.com>
2017-10-10 23:58:59 -0600

    Main HTML Changes to main pages and Javascript on LogIn

--- Module 15 - Capstone/Project 15/js/main.js
+++ Module 15 - Capstone/Project 15/js/main.js
@@ -5,9 +5,10 @@ function logInFunction(){
 	var userUserName;
 	userUserName = document.getElementById("userNameUserInput").value;
 	console.log(userUserName);
-	if(userUserName != "" || userUserName != null){
+	if(userUserName != "" && userUserName != null){
 		//set classes to hide login and sign up buttons and display a welcome message
-		document.getElementById
+		document.getElementById("logInButton").className = "hidden";
+		document.getElementById("signUpButton").className = "hidden";
 	}
 }
 
