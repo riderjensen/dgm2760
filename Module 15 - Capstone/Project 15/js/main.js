3863a5473419958a8c4c1eab08f142aee2692b61
Unknown <riderjensen1@gmail.com>
2017-10-10 23:00:28 -0600

    Working On Nav, creating hide class

--- Module 15 - Capstone/Project 15/js/main.js
+++ Module 15 - Capstone/Project 15/js/main.js
@@ -1,4 +1,13 @@
+/*jslint browser:true */
+"use strict()";
 
-
-
+function logInFunction(){
+	var userUserName;
+	userUserName = document.getElementById("userNameUserInput").value;
+	console.log(userUserName);
+	if(userUserName != "" || userUserName != null){
+		//set classes to hide login and sign up buttons and display a welcome message
+		document.getElementById
+	}
+}
 
